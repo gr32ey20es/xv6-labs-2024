95604334a4fd8e1c55c106ea8a1cf6b2dfeac11d
HatCun <142104320+gr32ey20es@users.noreply.github.com>
2025-02-26 03:05:47 +0700

    System call tracing

--- kernel/proc.c
+++ kernel/proc.c
@@ -295,6 +295,9 @@ fork(void)
     return -1;
   }
   np->sz = p->sz;
+  
+  // copy trace mask.
+  np->trace_mask = p->trace_mask;
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
--- kernel/proc.h
+++ kernel/proc.h
@@ -104,4 +104,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  
+  // 0 if not enabled
+  uint64 trace_mask;
 };
